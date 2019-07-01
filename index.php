c711aac1e0dd5f8e94626faa08253e3ccbd418d5
Tomas Ilginis <tilginis@gmail.com>
2019-07-01 23:00:32 +0300

    establishes connection to mysql

--- index.php
+++ index.php
@@ -3,5 +3,14 @@
 require_once 'vendor/autoload.php';
 
 echo (new \Rivsen\Demo\Hello('Docker'))->hello();
+
+// Create connection
+$conn = new mysqli('db', 'root', 'root', 'test_db', 3306);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection to mysql failed: " . $conn->connect_error);
+}
+
 echo phpinfo();
 
